133b0291e426d1d00e89858ac6fb465e84c30e9e
KawakamiMotoki <kawakami_m@mti.co.jp>
2012-07-27 17:26:29 +0900

    色対応

--- src/com/example/sample2/MainActivity.java
+++ src/com/example/sample2/MainActivity.java
@@ -1,24 +1,26 @@
 package com.example.sample2;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		TextView textView = (TextView) findViewById(R.id.textView);
+		textView.setTextColor(Color.RED);
+	}
 
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
 }
